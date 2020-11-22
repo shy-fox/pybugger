860597d194403276b77d2cebbbdb50b5c0ed78d7
ֆɦɨʀօʍɨ <42133415+shy-fox@users.noreply.github.com>
2020-11-22 18:03:27 +0000

    Create debug.py

---
+++ debug.py
@@ -0,0 +1,44 @@
+from  typing import Dict
+from enum import Enum
+from datetime import datetime as dt
+
+class Debug:
+	
+	class Formatter(Enum):
+		LOG = 'log',
+		ERROR = 'error',
+		FUNCTION = 'function',
+		VARIABLE = 'var',
+		DEBUG = 'debug',
+		PROGRAM = 'result'
+	
+	def variable(self, name: str, value: any) -> None:
+		print("[VARIABLE]: %s = %s" % (name, value))
+	
+	def function(self, name: str, args: Dict[str, any]) -> None:
+		_args = ''
+		
+		for name, val in args.items():
+			_args += "%s: %s," % (name, val)
+		
+		_args = _args[:len(_args) - 1]
+		
+		print("[FUNCTION]: Executed '%s' with '%s' as arguments." % (name, _args))
+	
+	def log(self, text: str) -> None:
+		print("[LOG]: %s" % text)
+	
+	def format(self, mode: Formatter, time=False) -> str:
+		_mode = mode.value if isinstance(mode.value, str) else mode.value[0]
+		_now = dt.now()
+		_hr = '0%s' % _now.hour if _now.hour < 10 else _now.hour
+		_min = '0%s' % _now.minute if _now.minute < 10 else _now.minute
+		_sec = '0%s' % _now.second if _now.second < 10 else _now.second
+		_time = "%s:%s:%s" %(_hr, _min, _sec)
+		_return = _time if time else ''
+		_return += ' [%s]' % _mode.upper()
+		
+		return _return
+		
+s = Debug().format(Debug.Formatter.FUNCTION, True)
+print(s)
